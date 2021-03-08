f1200a269c62c1dd62bedeeb11ffe8e12e73d348
Kevin Craig <kevinduboiscraig@gmail.com>
2021-03-08 18:53:59 -0500

    Implemented first pass of the UI/Scene system. Loading screen between scene loads MainMenu, HUD, Pause are working. Need to block player controls when the game is paused (will be done with the player controller)
    Need to implement a Game Manager in the Core scene for.
    
    All UIs need design inspiration, they are very basic.

---
+++ DesignWeek2020/Assets/Scripts/Systems/MenuSystem/MenuManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : Singleton<MenuManager>
+{
+	private Dictionary<string, Menu> MenuList = new Dictionary<string, Menu>();
+
+	public T GetMenu<T>(MenuClassifier menuClassifier) where T : Menu
+	{
+		Menu menu;
+		if (MenuList.TryGetValue(menuClassifier.menuName, out menu))
+		{
+			return (T)menu;
+		}
+		return null;
+	}
+
+	public void AddMenu(Menu menu)
+	{
+		if (MenuList.ContainsKey(menu.menuClassifier.menuName))
+		{
+			Debug.Assert(false, $"Menu is already registered {menu.menuClassifier.menuName}");
+		}
+		MenuList.Add(menu.menuClassifier.menuName, menu);
+	}
+
+	public void Remove(Menu menu)
+	{
+		MenuList.Remove(menu.menuClassifier.menuName);
+	}
+
+	public void ShowMenu(MenuClassifier classifier, string options = "")
+	{
+		Menu menu;
+		if (MenuList.TryGetValue(classifier.menuName, out menu))
+		{
+			Debug.Log($"Showing {classifier.menuName}");
+			menu.OnShowMenu(options);
+			return;
+		}
+	}
+
+	public void HideMenu(MenuClassifier classifier, string options = "")
+	{
+		Menu menu;
+		if (MenuList.TryGetValue(classifier.menuName, out menu))
+		{
+			Debug.Log($"Hiding {classifier.menuName}");
+			menu.OnHideMenu(options);
+			return;
+		}
+	}
+}
