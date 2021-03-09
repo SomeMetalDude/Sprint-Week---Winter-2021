e778a0d0c7f34bd1ac6ecb94e03c446f674cf8a5
Kevin Craig <kevinduboiscraig@gmail.com>
2021-03-09 17:17:56 -0500

    +GameManager GameManager keeps track of time Timer begins at GameScene load GameManager will send out events when the timer is depleted (Will use this to reset values in the scene and the timer)

---
+++ DesignWeek2020/Assets/Scripts/Systems/Core/GameManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameManager : Singleton<GameManager>
+{
+    public float secondsUntilReset = 300; // 5 Mins
+    public float secondsLeft;
+
+    private bool gamePaused = true;
+
+    public UnityEvent timeElapsedEvent = new UnityEvent();
+
+    public MenuClassifier pauseMenuClassifier;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetCountdown();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gamePaused)
+        {
+            secondsLeft -= Time.deltaTime;
+            if (secondsLeft <= 0)
+            {
+                timeElapsedEvent.Invoke();
+            }
+        }
+    }
+
+    public void ResetCountdown()
+    {
+        secondsLeft = secondsUntilReset;
+    }
+    
+    public void SetGamePaused(bool paused)
+    {
+        if (paused)
+        {
+            MenuManager.Instance.ShowMenu(pauseMenuClassifier);
+            gamePaused = true;
+        }
+        else
+        {
+            MenuManager.Instance.HideMenu(pauseMenuClassifier);
+            gamePaused = false;
+        }
+    }
+
+    public bool isGamePaused()
+    {
+        return gamePaused;
+    }
+}
