cd19ab5647ff41b978a9f5058fe3629edbd9f2a4
Kevin Craig <kevinduboiscraig@gmail.com>
2021-03-11 11:21:01 -0500

    Small change to ToggleItem, events now fire in the parent class, makes for easier creation of triggers

--- DesignWeek2020/Assets/Scripts/Systems/Interaction/Abstract/ToggleItem.cs
+++ DesignWeek2020/Assets/Scripts/Systems/Interaction/Abstract/ToggleItem.cs
@@ -1,9 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+
+public class ToggleItemStateChangedEvent : UnityEvent<bool> { }
 
 public abstract class ToggleItem : InteractableObject
 {
     public bool isOn = true;
-    
+
+    public ToggleItemStateChangedEvent toggleItemStateChangedEvent = new ToggleItemStateChangedEvent(); //Every turret will have to listen to this
+
+    public override void Interact()
+    {
+        isOn = !isOn;
+        toggleItemStateChangedEvent.Invoke(isOn);
+    }
 }
