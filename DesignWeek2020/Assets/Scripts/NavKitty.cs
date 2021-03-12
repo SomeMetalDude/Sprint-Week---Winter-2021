0302acbc9296a32f459f445b9454e64b370c156f
some_metal_dude <rosalesc@sheridancollege.ca>
2021-03-12 00:43:51 -0500

    Added more working Roombas. Create a Cat AI that follows the player around.

---
+++ DesignWeek2020/Assets/Scripts/NavKitty.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavKitty : MonoBehaviour
+{
+    Transform player;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        if (!player)
+        {
+            Debug.Log("Player not found!");
+            return;
+        }
+    }
+
+    void Update()
+    {
+        agent.destination = player.position;
+    }
+}
