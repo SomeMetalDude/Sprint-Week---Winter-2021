00f8596491bea57fa5ede6c71758597132ff7a5f
some_metal_dude <rosalesc@sheridancollege.ca>
2021-03-11 22:43:04 -0500

    Sound Implementation merge.

---
+++ DesignWeek2020/Assets/Scripts/Character Controller/Footsteps.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string inputSound;
+    [SerializeField] bool playerIsMoving;
+    public float walkingSoundSpeed;
+
+    void Start()
+    {
+        InvokeRepeating("PlayFootsteps", 0, walkingSoundSpeed);
+    }
+
+    void Update()
+    {
+        if (Input.GetAxis("Vertical") >= 0.01f || Input.GetAxis("Vertical") <= -0.01f 
+            || Input.GetAxis("Horizontal") >= 0.01f || Input.GetAxis("Horizontal") <= -0.01f)
+        {
+            playerIsMoving = true;
+        }
+        else playerIsMoving = false;
+    }
+
+    void PlayFootsteps()
+    {
+        if (playerIsMoving == true)
+        {
+            FMODUnity.RuntimeManager.PlayOneShot(inputSound);
+        }
+    }
+}
