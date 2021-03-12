cdb19eeb967e979d33523ef49d602f27f253f86d
some_metal_dude <rosalesc@sheridancollege.ca>
2021-03-12 01:24:49 -0500

    Create a title screen scene.

---
+++ DesignWeek2020/Assets/Scripts/KittySpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KittySpawner : MonoBehaviour
+{
+    public bool conditionsMet;
+    [SerializeField] GameObject kittyPrefab;
+    [SerializeField] float repeatRate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnHappinness", 0, repeatRate);
+    }
+
+    void SpawnHappinness()
+    {
+        if (conditionsMet) Instantiate(kittyPrefab, this.transform);
+    }
+}
---
+++ DesignWeek2020/Assets/TitleScreenScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenScript : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            LoadNextScene();
+        }
+    }
+
+    void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
